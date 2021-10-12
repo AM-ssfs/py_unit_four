38d3d6efe5fa8378432e5bb3e21e6aa1bd2ad8b9
AM-ssfs <menscad@ssfs.org>
2021-10-12 12:59:45 -0400

    assignment_four more doc str and comments

--- assignment_four.py
+++ assignment_four.py
@@ -1,11 +1,16 @@
+# Aden Menschik
+# Last modified Oct. 12, 2021
+# This program helps workers at The Adventure Park determine the price of a ticket
+
+
 def simplify_county(county):
     """
-    checks for variations of spellings so it works wor
+    checks for variations of county spellings
     simplifies here i don't have to check variations of spelling later
-    :param county:
-    what county they are in (str)
-    :return abbreviated county:
-    abbreviated county (first letter) (str)
+    :param county: (str)
+    what county they are in
+    :return abbreviated county: (str)
+    abbreviated county (first letter)
     """
     county = str(county).upper()
     if county == "MONTGOMERY COUNTY" or county == "MONTGOMERY" or county == "MONT" or county == "MG" or county == "M":
@@ -20,8 +25,8 @@ def simplify_county(county):
 
 def get_age_county():
     """
-    gets age and county and returns them together as a list
-    :return age_county list[ age(int), county(str) ]:
+    gets age and county, abbreviates county, then returns them together as a list
+    :return age_county: list[ age(int), county(str) ]
     returns them in a list so i can find them both easily and i don't have to get another user input
     """
     age = int(input("How old are you? "))
@@ -33,11 +38,11 @@ def get_age_county():
 def discount(price, discount):
     """
     easily used to calculate discounts, helpful later where i do a bunch of them
-    :param price (float):
+    :param price: (float)
     the price before i discount it
-    :param discount (float):
-    the amount of discount (ex. 15% off would be shown as 15)
-    :return new_price (float):
+    :param discount: (float)
+    the amount of discount (ex. 17.5% off would be shown as 17.5)
+    :return new_price: (float)
     the price after the discount
     """
     new_price = (price * (1-(discount/100)))
@@ -46,12 +51,12 @@ def discount(price, discount):
 
 def get_cost(age, county):
     """
-    the actual price evaluating is done here, returns the *final* price (may not be properly formatted)
-    :param age (int):
+    the actual price evaluating is done here, returns the 'final' price (however it may not be properly formatted)
+    :param age: (int)
     user's age
-    :param county (str):
-    user's county
-    :return new_price (float):
+    :param county: (str)
+    user's county (abbreviated)
+    :return new_price: (float)
     ticket price based off of age and county
     """
     base_price = 70
@@ -85,9 +90,9 @@ def format_cost(price):
     """
     turns the price into string and into proper currency format (ex. 52.80)
     check number of decimals and adds 0's or rounds to nearest hundredth if needed
-    :param price (float):
+    :param price: (float)
     the "final" price from get_cost() before it is formatted correctly
-    :return new_price (str):
+    :return new_price: (str)
     the real final price after formatting it and making it a string
     """
 
